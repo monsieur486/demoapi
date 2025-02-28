f92ac227ec431c6262c60c3e01ea0bec56648fb6
monsieur486 <monsieur486@gmail.com>
2025-02-28 12:17:49 +0100

    first commit

---
+++ src/main/java/com/mr486/demoapi/DemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.mr486.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApiApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/mr486/demoapi/MainController.java
@@ -0,0 +1,33 @@
+package com.mr486.demoapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class MainController {
+
+  @GetMapping("/")
+  public Map<String, String> greet(@RequestParam(name = "nom", required = false) String name) {
+    String message = "";
+    String color= "";
+
+    if(name != null && !name.isEmpty()) {
+      if(name.equals("darling")) {
+        message = "❤\uFE0F ❤\uFE0F ❤\uFE0F Coucou mon Amour ❤\uFE0F ❤\uFE0F ❤\uFE0F";
+        color = "red";
+      } else {
+        message = "Bonjour " + name + "!";
+        color = "green";
+      }
+
+    } else {
+      message = "Hello World!";
+      color = "black";
+    }
+    return Map.of("message", message, "color", color);
+  }
+
+}
